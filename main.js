386ad910be4d28103fb04645de8f5e016072d574
Victor Caldas <victor.solid3@gmail.com>
2020-03-11 15:19:30 -0300

    dropdown produtos

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
 const sectionSobre = document.querySelector('#sobre');
-
+const trackWindow = window.innerHeight * 0.6;
 window.onload = function animaScroll(event) {
 
   sectionSobre.classList.add('ativo');
 
 }
+
+function animaProdutos() {
+  const divProdutos = document.querySelectorAll('.produto-item');
+
+  function animaProdutos() {
+    divProdutos.forEach((div) => {
+      const divTop = div.getBoundingClientRect().top;
+      const divHeight = (divTop - trackWindow) < 0;
+      if (divHeight) {
+        div.classList.add('ativo');
+      }
+    })
+  }
+
+  window.addEventListener('scroll', animaProdutos);
+}
+animaProdutos();
